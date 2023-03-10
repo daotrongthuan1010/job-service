030f16b6bd90f2e9a8a237cf1ed87b0f281ec886
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-03-10 23:45:14 +0700

    update repository

---
+++ src/main/java/com/job/future/jobservice/dto/GetCommentByPostDto.java
@@ -0,0 +1,20 @@
+package com.job.future.jobservice.dto;
+
+import java.time.LocalDateTime;
+import lombok.Data;
+
+/**
+ * @author thuandao1010
+ * @version 1.0
+ * @since 2023-02-11
+ */
+@Data
+public class GetCommentByPostDto {
+
+  private String id;
+
+  private String content;
+
+  private LocalDateTime updatedAt;
+
+}
